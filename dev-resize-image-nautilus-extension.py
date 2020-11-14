277d7a94ed6e8663eda62a0284ff7fb5da19c324
yucef sourani <yucefsourani@users.noreply.github.com>
2020-11-14 19:56:15 +0200

    Add files via upload

---
+++ dev-resize-image-nautilus-extension.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  dev-resize-image-nautilus-extension.py
+#  
+#  Copyright 2020 yucef sourani <youssef.m.sourani@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+# 
+import gi
+gi.require_version('GdkPixbuf', '2.0')
+from gi.repository import Nautilus, GObject, Gio, GdkPixbuf
+import os
+
+SUPPORTED_FORMATS = dict([(i.get_mime_types()[0],i.get_name()) for i in GdkPixbuf.Pixbuf.get_formats() if i]+[("image/jpg","jpg",)])
+SIZE = ("8x8","16x16","22x22","24x24","32x32","64x64","96x96","128x128","256x256","512x512")
+
+
+class BackgroundImageExtension(GObject.GObject, Nautilus.MenuProvider):
+    
+    def get_file_items(self, window, files):
+        if len(files) != 1:
+            return
+
+        file = files[0]
+
+        mime_type = file.get_mime_type()
+        if not  mime_type in SUPPORTED_FORMATS.keys():
+            return
+
+        if file.get_uri_scheme() != 'file':
+            return
+            
+        menu = Nautilus.Menu.new()
+        item = Nautilus.MenuItem(name='Nautilus::Resize_Image_',
+                                 label='Resize Image',
+                                 tip='Resize Image')
+        item.set_submenu(menu)
+        
+        for size in SIZE:
+            item2 = Nautilus.MenuItem(name='Nautilus::set_image_size_'+size,
+                                 label=size,
+                                 tip='Resize current image to '+size)
+        
+            menu.append_item(item2)
+            item2.connect('activate', self.resize_and_save, file,size,mime_type)
+
+        item2 = Nautilus.MenuItem(name='Nautilus::set_image_size_all',
+                            label="all",
+                            tip='Resize current image to all size')
+        
+        menu.append_item(item2)
+        item2.connect('activate', self.resize_and_save, file,"all",mime_type)
+        return item,
+
+    # Current versions of Nautilus will throw a warning if get_background_items
+    # isn't present
+    def get_background_items(self, window, file):
+        return None
+
+    def resize_and_save(self,menu,file,size,mime_type,ignore_aspect_ration=False):
+        if size == "all":
+            try :
+                image = file.get_location().get_path()
+                parent_location = file.get_parent_location().get_path()
+                for s in SIZE:
+                    folder_location = os.path.join(parent_location,"hicolor" ,s,"apps")
+                    os.makedirs(folder_location, exist_ok=True)
+                    width,height= s.split("x")
+                    saveas = os.path.join(folder_location,file.get_name())
+                    im = GdkPixbuf.Pixbuf.new_from_file_at_scale(image,int(width),int(height),ignore_aspect_ration)
+                    if not im.savev(saveas,SUPPORTED_FORMATS[mime_type],[],[]):
+                        return False
+                    print(saveas)
+            except Exception as e:
+                print("ERROR: {}.\nResize: {} Faild".format(e,image))
+                return False
+        else:
+            try:
+                folder_location = os.path.join(file.get_parent_location().get_path(),"hicolor" ,size,"apps")
+                image =  file.get_location().get_path()
+                os.makedirs(folder_location, exist_ok=True)
+                width,height= size.split("x")
+                saveas = os.path.join(folder_location,file.get_name())
+                im = GdkPixbuf.Pixbuf.new_from_file_at_scale(image,int(width),int(height),ignore_aspect_ration)
+                if not im.savev(saveas,SUPPORTED_FORMATS[mime_type],[],[]):
+                    return False
+                print(saveas)
+            except Exception as e:
+                print("ERROR: {}.\nResize: {} Faild".format(e,image))
+                return False
+        return saveas
